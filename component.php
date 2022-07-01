1c5e2eabb55d2ed4d6611fa75898efe442b935f7
Cameron Jones <cameron@cameronjonesweb.com.au>
2022-07-01 10:32:09 +0930

    Version 2

--- component.php
+++ component.php
@@ -5,7 +5,7 @@
  * @package WP_Theme_Components
  * @subpackage remove-prefix-from-archive-titles
  * @link https://github.com/WP-Theme-Components/remove-prefix-from-archive-titles
- * @version 1.0.0
+ * @version 2.0.0
  * @author Cameron Jones
  */
 
@@ -17,11 +17,11 @@ namespace WP_Theme_Components\Remove_Prefix_From_Archive_Titles;
  * @link https://stackoverflow.com/a/22997249/1672694
  * @since 1.0.0
  * @param string $title The archive title.
+ * @param string $original Original archive title (without prefixes).
  * @return string
  */
-function remove_archive_prefix( $title ) {
-	$title = trim( substr( $title, strpos( $title, ':' ) + 1 ) );
-	return $title;
+function remove_archive_prefix( $title, $original ) {
+    return $original;
 }
 
-add_filter( 'get_the_archive_title', __NAMESPACE__ . '\\remove_archive_prefix' );
+add_filter( 'get_the_archive_title', __NAMESPACE__ . '\\remove_archive_prefix', 10, 2 );
